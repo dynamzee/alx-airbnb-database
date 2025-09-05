e6d20ec2640c3b71750b655940e457fc72dc912a
dynamzee <dynamzee@gmail.com>
2025-09-05 15:06:08 +0100

    add my subqueries.sql

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,17 @@
+USE airbnb_db;
+SELECT *
+FROM
+	(SELECT AVG(rating) as av_rating, property_id
+	FROM review r
+	GROUP BY property_id)t
+    
+WHERE av_rating > "4.0"
+
+USE airbnb_db;
+SELECT *
+FROM 
+	(SELECT COUNT(booking_id) total, user_id
+	FROM booking 
+	GROUP BY user_id)t
+    
+WHERE total > 3
